fd4b44d74761237f12c3dc4c9f8ce9cad4876c58
Jolan Cornevin <jcornevin@work4labs.com>
2018-09-04 10:12:48 +0200

    indend the pdf code with 4 spaces

--- aws/src/handlers/pdf.js
+++ aws/src/handlers/pdf.js
@@ -9,35 +9,36 @@ import log from '../utils/log'
 import pdf, { makePrintOptions } from '../chrome/pdf'
 
 export default async function handler (event, context, callback) {
-  const queryStringParameters = event.queryStringParameters || {}
-  const {
-    url = 'https://github.com/adieuadieu/serverless-chrome',
-    ...printParameters
-  } = queryStringParameters
-  const printOptions = makePrintOptions(printParameters)
-  let data
+    const queryStringParameters = event.queryStringParameters || {}
+    const {
+        url,
+        ...printParameters
+    } = queryStringParameters
 
-  log('Processing PDFification for', url, printOptions)
+    const printOptions = makePrintOptions(printParameters)
+    let data;
 
-  const startTime = Date.now()
+    log('Processing PDFification for', url, printOptions)
 
-  try {
-    data = await pdf(url, printOptions)
-  } catch (error) {
-    console.error('Error printing pdf for', url, error)
-    return callback(error)
-  }
+    const startTime = Date.now()
 
-  log(`Chromium took ${Date.now() - startTime}ms to load URL and render PDF.`)
+    try {
+        data = await pdf(url, printOptions)
+    } catch (error) {
+        console.error('Error printing pdf for', url, error)
+        return callback(error)
+    }
 
-  // TODO: handle cases where the response is > 10MB
-  // with saving to S3 or something since API Gateway has a body limit of 10MB
-  return callback(null, {
-    statusCode: 200,
-    body: data,
-    isBase64Encoded: true,
-    headers: {
-      'Content-Type': 'application/pdf',
-    },
-  })
+    log(`Chromium took ${Date.now() - startTime}ms to load URL and render PDF.`)
+
+    // TODO: handle cases where the response is > 10MB
+    // with saving to S3 or something since API Gateway has a body limit of 10MB
+    return callback(null, {
+        statusCode: 200,
+        body: data,
+        isBase64Encoded: true,
+        headers: {
+            'Content-Type': 'application/pdf',
+        },
+    })
 }
